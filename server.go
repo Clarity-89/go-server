d2d83de5f39c7300a5742e1096b49f958d2b7a90
Clarity-89 <homes89@ukr.net>
2020-07-09 11:23:24 +0300

    Add template

--- server.go
+++ server.go
@@ -11,10 +11,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Test")
 }
 
-const port = "8000"
+func todos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Todos")
+}
+
+const port = "8080"
 
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/todos", todos)
 	fmt.Println("Running on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }

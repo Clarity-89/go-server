6a48db7d9cb7967ec9b24d79421980d1ec2cb591
Clarity-89 <homes89@ukr.net>
2020-07-09 13:25:53 +0300

    Save todos

--- server.go
+++ server.go
@@ -7,27 +7,67 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Test")
+const port = "8080"
+
+type Todo struct {
+	Title   string
+	Content string
 }
 
-func todos(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/todos.html")
+type PageContext struct {
+	Title string
+	Todos []Todo
+}
+
+var todos []Todo
+
+func home(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Print("Template parsing error:", err)
 	}
 
 	err = t.Execute(w, nil)
-
 }
 
-const port = "8080"
+func listTodos(w http.ResponseWriter, r *http.Request) {
+	pageContext := PageContext{
+		Todos: todos,
+		Title: "Todos",
+	}
+
+	t, err := template.ParseFiles("templates/todos.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Template parsing error:", err)
+	}
+
+	err = t.Execute(w, pageContext)
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Form parsing error:", err)
+	}
+
+	todo := Todo{
+		Title:   r.FormValue("title"),
+		Content: r.FormValue("content"),
+	}
+
+	todos = append(todos, todo)
+
+	http.Redirect(w, r, "/todos/", http.StatusSeeOther)
+}
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", home)
-	http.HandleFunc("/todos", todos)
+	http.HandleFunc("/todos/", listTodos)
+	http.HandleFunc("/add-todo", addTodo)
 	fmt.Println("Running on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
